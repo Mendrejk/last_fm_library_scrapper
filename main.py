81d0894e069f25c868fc91117b5ad461bbdcb113
Mendrejk <bastian.lakomy@gmail.com>
2022-12-13 00:44:24 +0100

    Add typing hints

--- main.py
+++ main.py
@@ -2,38 +2,41 @@ import requests
 from bs4 import BeautifulSoup
 
 from collections import OrderedDict
+from typing import List, Tuple, Any
+
+from requests import Response
 
 
-def get_soup(endpoint):
-    __user_library_url = 'https://www.last.fm/user/Maendrake/library/'
+def get_soup(endpoint: str) -> BeautifulSoup:
+    __user_library_url: str = 'https://www.last.fm/user/Maendrake/library/'
 
     # Make a GET request to the URL and retrieve the HTML content
-    response = requests.get(__user_library_url + endpoint)
-    html_content = response.text
+    response: Response = requests.get(__user_library_url + endpoint)
+    html_content: str = response.text
 
     # Use BeautifulSoup to parse the HTML content and extract the relevant information
     return BeautifulSoup(html_content, 'html.parser')
 
 
-def scrap_tracks():
-    soup = get_soup('tracks')
-    songs = [s.text.strip() for s in soup.select('.chartlist-name')]
-    artists = [r.text.strip() for r in soup.select('.chartlist-artist')]
+def scrap_tracks() -> Tuple[List[str], List[str]]:
+    soup: BeautifulSoup = get_soup('tracks')
+    songs: list[str] = [s.text.strip() for s in soup.select('.chartlist-name')]
+    artists: list[str] = [r.text.strip() for r in soup.select('.chartlist-artist')]
 
     return songs, artists
 
 
-def scrap_albums():
-    soup = get_soup('albums')
-    albums = [a.text.strip() for a in soup.select('.chartlist-name')]
-    artists = [a.text.strip() for a in soup.select('.chartlist-artist')]
+def scrap_albums() -> Tuple[List[str], List[str]]:
+    soup: BeautifulSoup = get_soup('albums')
+    albums: list[str] = [a.text.strip() for a in soup.select('.chartlist-name')]
+    artists: list[str] = [a.text.strip() for a in soup.select('.chartlist-artist')]
 
     return albums, artists
 
 
-def scrap_artists():
-    soup = get_soup('artists')
-    artists = [a.text.strip() for a in soup.select('.chartlist-name')]
+def scrap_artists() -> list[str]:
+    soup: BeautifulSoup = get_soup('artists')
+    artists: list[str] = [a.text.strip() for a in soup.select('.chartlist-name')]
 
     return artists
 
@@ -44,9 +47,9 @@ def main():
     artists = scrap_artists()
 
     # Create ordered dictionaries to store the unique items from each list
-    unique_songs = OrderedDict.fromkeys(songs)
-    unique_albums = OrderedDict.fromkeys(albums)
-    unique_artists = OrderedDict.fromkeys(artists + artists_from_albums + artists)
+    unique_songs: OrderedDict[str, None] = OrderedDict.fromkeys(songs)
+    unique_albums: OrderedDict[str, None] = OrderedDict.fromkeys(albums)
+    unique_artists: OrderedDict[str, None] = OrderedDict.fromkeys(artists + artists_from_albums + artists)
 
     print(unique_artists.keys())
 
